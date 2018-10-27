2deccd71ee0905514172e3d8bb636e5ad79763dd
Stypox <stypox@pm.me>
2018-10-27 14:47:22 +0200

    Implemented parser::Sentence::score()

--- src/parser/sentence.cpp
+++ src/parser/sentence.cpp
@@ -1,13 +1,52 @@
 #include "sentence.h"
 
+#include <algorithm>
+#include <iostream>
+
 namespace parser {
 	Sentence::Sentence(const std::vector<std::string>& words, const std::string& code) :
-		m_code{code}, m_words{words} {}
+		m_words{words}, m_code{code} {}
 
-	int Sentence::score(const std::vector<std::string>& words) const {
+	int Sentence::score(const std::vector<std::string>& compareWords) const {
+		int points = pointsAtBeginning;
+		bool foundAllWords = true,
+			exactMatch = true;
+		auto wordsBegin = m_words.begin();
 
+
+		for (auto compareWord = compareWords.begin(); compareWord != compareWords.end(); ++compareWord) {
+			if (wordsBegin == m_words.end()) {
+				foundAllWords = false;
+				points += std::max(-pointsFoundWord, static_cast<int>(compareWords.end() - compareWord) * pointsWordsInMiddle);
+				break;
+			}
+
+			auto foundWord = std::find(wordsBegin, m_words.end(), *compareWord);
+
+			if (foundWord == m_words.end()) {
+				foundAllWords = false;
+			}
+			else {
+				points += pointsFoundWord;
+
+				if (foundWord != wordsBegin) {
+					exactMatch = false;
+					points += std::max(-pointsFoundWord, static_cast<int>(foundWord - wordsBegin) * pointsWordsInMiddle);
+				}
+
+				wordsBegin = foundWord + 1;
+			}
+		}
+
+		if (foundAllWords) {
+			points += pointsFoundAllWords;
+			if (exactMatch)
+				points += pointsExactMatch;
+		}
+
+		return points;
 	}
 	void Sentence::exec() const {
-		
+		std::cout << m_words[0];
 	}
 }
--- src/parser/sentence.h
+++ src/parser/sentence.h
@@ -5,13 +5,19 @@
 #include <string>
 
 namespace parser {
+	constexpr int pointsAtBeginning = 0,
+		pointsFoundWord = 4,
+		pointsWordsInMiddle = -1,
+		pointsFoundAllWords = 10,
+		pointsExactMatch = 5;
+
 	class Sentence {
 		const std::vector<std::string> m_words;
 		const std::string& m_code;
 	public:
 		Sentence(const std::vector<std::string>& words, const std::string& code);
 
-		int score(const std::vector<std::string>& words) const;
+		int score(const std::vector<std::string>& compareWords) const;
 		void exec() const;
 	};
 }
