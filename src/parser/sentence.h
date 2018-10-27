5da5c42dd07df75f213903bcc2f3c1a543c40366
Stypox <stypox@pm.me>
2018-10-27 13:53:17 +0200

    Made Sentence much simple but not compile-time.

--- src/parser/sentence.h
+++ src/parser/sentence.h
@@ -6,17 +6,11 @@
 #include <string>
 
 namespace parser {
-	template<typename Container, typename = typename std::enable_if_t<
-		std::is_same_v<typename Container::value_type, std::string> ||
-		std::is_same_v<typename Container::value_type, std::string_view>>>
-	class SentenceBase {
+	class Sentence {
+		const std::vector<std::string> m_words;
+		const std::string& m_code;
 	public:
-		using string_type = std::conditional_t<std::is_literal_type_v<Container>, std::string_view, std::string>;
-	private:
-		const string_type m_code;
-		const Container m_words;
-	public:
-		constexpr SentenceBase(const string_type& code, const Container& words) :
+		Sentence(const std::vector<std::string>& words, const std::string& code) :
 			m_code{code}, m_words{words} {}
 
 		int score(const std::vector<std::string>& words) const {
@@ -26,19 +20,6 @@ namespace parser {
 
 		}
 	};
-
-	template<size_t N>
-	using ConstSentence = SentenceBase<std::array<std::string_view, N>>;
-	using Sentence = SentenceBase<std::vector<std::string>>;
-
-	template <typename N>
-	struct is_sentence : std::false_type {};
-	template <>
-	struct is_sentence<Sentence> : std::true_type {};
-	template <size_t N>
-	struct is_sentence<ConstSentence<N>> : std::true_type {};
-	template <typename T>
-	inline constexpr bool is_sentence_v = is_sentence<T>::value;
 }
 
 #endif
