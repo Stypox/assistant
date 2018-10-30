a83b8cde5dffe4d288306394a8503b31a2185fd8
Stypox <stypox@pm.me>
2018-10-30 22:28:50 +0100

    Implemented parser::Compiler::toCpp()
    It still doesn't include CapturingSentence's

--- sentences-compiler/src/parser/compiler.cpp
+++ sentences-compiler/src/parser/compiler.cpp
@@ -11,6 +11,20 @@ namespace parser {
 	}
 
 	void Compiler::toCpp(std::ostream& output) {
-		
+		constexpr std::string_view codeObjectName = "code_of_section_";
+		constexpr std::string_view parserObjectName = "parser";
+
+		output << "namespace sentences_compiler_gen {\n";
+
+		for (size_t sectionPos = 0; sectionPos != m_sections.size(); ++sectionPos)
+			output << "std::string " << codeObjectName << sectionPos + 1 << "{" << m_sections[sectionPos].cppCodeStringLiteral() << "};\n";
+
+		output << "parser::Parser " << parserObjectName << "{{";
+		if (m_sections.size() != 0)
+			output << m_sections[0].cppSentencesList(std::string{codeObjectName} + "1");
+		for (size_t sectionPos = 1; sectionPos != m_sections.size(); ++sectionPos)
+			output << ",\n" << m_sections[sectionPos].cppSentencesList(std::string{codeObjectName} + std::to_string(sectionPos + 1));
+
+		output << "},\n" << m_codeWhenNotUnderstood.cppStringLiteral() << "};\n}";
 	}
 }
