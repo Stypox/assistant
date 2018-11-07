98de290248b18802801bfc90ece0de1bf5b57a41
Stypox <stypox@pm.me>
2018-11-07 21:21:21 +0100

    Removed unused code from sentences-compiler.

--- sentences-compiler/src/parser/constructs/code.cpp
+++ sentences-compiler/src/parser/constructs/code.cpp
@@ -1,27 +1,9 @@
 #include "code.h"
 
 namespace parser::constructs {
-	using std::string;
-
-	void replaceAll(Code& oldCode, const string& before, const string& after) {		
-		Code newCode;
-		newCode.reserve(oldCode.length());
-
-		string::size_type findPosLast = 0,
-			findPos;
-		while((findPos = oldCode.find(before, findPosLast)) != string::npos) {
-			newCode.append(oldCode, findPosLast, findPos - findPosLast);
-			newCode += after;
-			findPosLast = findPos + before.length();
-		}
-
-		newCode += oldCode.substr(findPosLast);
-		newCode.swap(oldCode);
-	}
-
-	string Code::cppStringLiteral() const {
-		static const string codeDelimiter {"_Code_Delimiter_"};
-		string copy = *this;
+	std::string Code::cppStringLiteral() const {
+		static const std::string codeDelimiter {"_Code_Delimiter_"};
+		std::string copy = *this;
 		copy.insert(0, "R\"" + codeDelimiter + "(");
 		copy.append(")" + codeDelimiter + "\"");
 		return copy;
