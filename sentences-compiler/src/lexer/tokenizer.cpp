0de806ffbec781e1abc7958dac29c78d9ae9ce0e
Stypox <stypox@pm.me>
2018-11-08 15:00:35 +0100

    Now words can contain '
    It is useful to represent accent marks, umlauts...

--- sentences-compiler/src/lexer/tokenizer.cpp
+++ sentences-compiler/src/lexer/tokenizer.cpp
@@ -13,7 +13,8 @@ namespace lexer {
 	}
 	bool isLetter(char ch) {
 		return ((ch >= 'a' && ch <= 'z') ||
-				(ch >= 'A' && ch <= 'Z'));
+				(ch >= 'A' && ch <= 'Z') ||
+				(ch == '\''));
 	}
 	bool isOtherValid(char ch) {
 		return ((ch >= '0' && ch <= '9') ||
