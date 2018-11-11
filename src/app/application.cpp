aa1404505577b6b28d741aed660b2b5eda9e7af0
Stypox <stypox@pm.me>
2018-11-11 14:00:24 +0100

    Implemented the first part of the assistant application.

--- src/app/application.cpp
+++ src/app/application.cpp
@@ -1,5 +1,86 @@
 #include "application.h"
 
+#include <iostream>
+#include <fstream>
+
 namespace app {
-	
+	stypox::ArgParser Application::currentArgsInitializer{
+		"",
+		{
+			{"help", "prints the help screen and exits", {"-?", "-h", "--help"}},
+			{"quit", "quit", {"-q", "--quit"}}
+		}, {
+
+		}, {
+
+		}, {
+			{"sentences", "replaces the precompiled sentences (format: defId,defCodeHex;id,word word,codeHex;id,word word ,word word,codeHex;...)", {"-s=", "--sentences="}}
+		}
+	};
+
+	stypox::ArgParser Application::initialArgs{
+		"assistant",
+		{
+			{"help", "prints the help screen and exits", {"-?", "-h", "--help"}}
+		}, {
+
+		}, {
+
+		}, {
+			{"output", "the output file (defaults to stdout)", {"-o=", "--output="}, "stdout"},
+			{"encoding", "input encoding (values: 8bit (default), 2x8bit (= 16bit split in two 8bit))", {"-e=", "--encoding="}, "8bit", [](std::string s) { return s == "8bit" || s == "2x8bit"; }},
+			{"format", "output format (values: json (default))", {"-f=", "--format="}, "json", [](std::string s) { return s == "json"; }},
+			{"logs", "the file to save logs in (when openable) (stdout is valid)", {"-l=", "--logs="}}
+		}
+	};
+	std::unique_ptr<std::ostream> Application::logs{};
+
+
+	bool Application::parseInitialArgs(int argc, char const *argv[]) {
+		try {
+			initialArgs.parse(argc, argv);
+			
+			if (std::string logsFilename = initialArgs.getText("logs"); logsFilename == "stdout") {
+				logs.reset(&std::cout);
+			}
+			else if (logsFilename != "") {
+				auto file = new std::ofstream{logsFilename, std::ios::binary};
+				if (file->is_open())
+					logs.reset(file);
+			}
+
+			if (initialArgs.getBool("help")) {
+				std::cout << initialArgs.help();
+				return false;
+			}
+			
+			initialArgs.validate();
+		}
+		catch (const std::runtime_error& e) {
+			if (logs.get() != nullptr)
+				*logs << "Error while parsing arguments: " << e.what();
+			return false;
+		}
+		
+		if (std::string outputFilename = initialArgs.getText("output"); outputFilename == "stdout") {
+			output.reset(&std::cout);
+		}
+		else {
+			auto file = new std::ofstream{outputFilename, std::ios::binary};
+			if (file->is_open())
+				output.reset(file);
+			else
+				return false;
+		}
+
+		return true;
+	}
+
+
+	int Application::run(int argc, char const *argv[]) {
+		if (!parseInitialArgs(argc, argv))
+			return !initialArgs.getBool("help");
+		
+		
+	}
 }
--- src/app/application.h
+++ src/app/application.h
@@ -2,11 +2,20 @@
 #define _VOICE_ASSISTANT_APP_APPLICATION_H_
 
 #include <argparser.h>
+#include <memory>
+#include <ostream>
 
 namespace app {
 	class Application {
+		static bool parseInitialArgs(int argc, char const *argv[]);
+		
+		static stypox::ArgParser currentArgsInitializer;
+
 	public:
-		static stypox::ArgParser args;
+		static stypox::ArgParser initialArgs;
+		static stypox::ArgParser currentArgs;
+		static std::unique_ptr<std::ostream> output;
+		static std::unique_ptr<std::ostream> logs;
 
 		static int run(int argc, char const *argv[]);
 	};
