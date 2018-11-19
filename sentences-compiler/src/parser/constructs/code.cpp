c4804ca18d2a3fbdda13661485d39c1ecf8bae2f
Stypox <stypox@pm.me>
2018-11-19 15:14:51 +0100

    Removed raw literal delimiters for empty id/code.

--- sentences-compiler/src/parser/constructs/code.cpp
+++ sentences-compiler/src/parser/constructs/code.cpp
@@ -3,6 +3,9 @@
 namespace parser::constructs {
 	std::string Code::cppStringLiteral() const {
 		static const std::string codeDelimiter {"_Code_Delimiter_"};
+		if (empty())
+			return "\"\"";
+
 		std::string copy = *this;
 		copy.insert(0, "R\"" + codeDelimiter + "(");
 		copy.append(")" + codeDelimiter + "\"");
--- sentences-compiler/src/parser/constructs/id.cpp
+++ sentences-compiler/src/parser/constructs/id.cpp
@@ -3,6 +3,9 @@
 namespace parser::constructs {
 	std::string Id::cppStringLiteral() const {
 		static const std::string idDelimiter {"__Id_Delimiter__"};
+		if (empty())
+			return "\"\"";
+
 		std::string copy = *this;
 		copy.insert(0, "R\"" + idDelimiter + "(");
 		copy.append(")" + idDelimiter + "\"");
